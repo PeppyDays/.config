797b4be75587549ae9225b0b2b1957e2781cb64d
Arine <peppydays@gmail.com>
2023-09-14 23:20:25 +0900

    Added just, yabai abbrs and fixed aws profile abbrs

--- fish/conf.d/abbreviations.fish
+++ fish/conf.d/abbreviations.fish
@@ -5,6 +5,8 @@ abbr --add kn kubens
 
 # aws sso
 abbr --add asl aws sso login --profile
+abbr --add aslh export AWS_PROFILE=hp-dev-prod-admin & aws sso login --profile hp-dev-prod-admin
+abbr --add asls export AWS_PROFILE=hp-shared-admin & aws sso login --profile hp-shared-admin
 
 # terraform
 abbr --add tf terraform
@@ -32,6 +34,13 @@ abbr --add zja zellij attach
 abbr --add cb cargo build
 abbr --add cr cargo run
 abbr --add ct cargo test
+
+# just
+abbr --add j just
+
+# yabai and skhd
+abbr --add ystop "yabai --stop-service; skhd --stop-service;"
+abbr --add ystart "yabai --start-service; skhd --start-service;"
 
 # favorite directories
 abbr --add cdp cd /Users/arine/Documents/Code/Project
