9856017d34d4c04c11fbc35e57d81144b7677fb5
Arine <arine@home.local>
2023-12-17 02:08:11 +0900

    Added theme config

--- nvim/lua/arine/lazy/plugins/colorscheme.lua
+++ nvim/lua/arine/lazy/plugins/colorscheme.lua
@@ -6,6 +6,24 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
+      require("catppuccin").setup({
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+              enabled = true,
+              indentscope_color = "",
+          },
+          mason = true,
+          telescope = {
+            enabled = true,
+          },
+        },
+      })
+
       vim.cmd.colorscheme 'catppuccin-frappe'
     end,
   },
