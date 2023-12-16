087916672b277aff45f67fa65b06d3e5e06a3e02
Arine <arine@home.local>
2023-12-17 03:37:53 +0900

    Fixed lualine filename config typo

--- nvim/lua/arine/lazy/plugins/lualine.lua
+++ nvim/lua/arine/lazy/plugins/lualine.lua
@@ -9,14 +9,14 @@ return {
       component_separators = '|',
       section_separators = '',
     },
-		sections = {
-			lualine_c = {
-				{
-					'filename',
-					path = 1,
-				},
-			},
-		},
+    sections = {
+      lualine_c = {
+        {
+          'filename',
+          path = 1,
+        },
+      },
+    },
   },
 }
 
