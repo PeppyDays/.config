a8c37e78a27a9c91701900f54bb85aa18b3f79eb
Arine <arine@home.local>
2023-12-18 02:12:41 +0900

    Added gitsigns instead of tpope's

--- nvim/lua/arine/lazy/plugins/git.lua
+++ nvim/lua/arine/lazy/plugins/git.lua
@@ -1,4 +1,7 @@
 return {
-  "tpope/vim-fugitive",
-  "tpope/vim-rhubarb",
+  -- "tpope/vim-fugitive",
+  -- "tpope/vim-rhubarb",
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = true,
 }
