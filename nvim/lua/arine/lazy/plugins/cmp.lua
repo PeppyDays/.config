284de3a93605ee4b3dde6f6b3b5f343d376602ca
Arine <arine@home.local>
2023-12-17 13:50:18 +0900

    Added LSP intermediately

--- nvim/lua/arine/lazy/plugins/cmp.lua
+++ nvim/lua/arine/lazy/plugins/cmp.lua
@@ -6,8 +6,8 @@ return {
     "L3MON4D3/LuaSnip",
     "saadparwaiz1/cmp_luasnip",
 
-		-- Adds completion from buffer
-		"hrsh7th/cmp-buffer",
+    -- Adds completion from buffer
+    "hrsh7th/cmp-buffer",
 
     -- Adds LSP completion capabilities
     "hrsh7th/cmp-nvim-lsp",
@@ -15,68 +15,68 @@ return {
 
     -- Adds a number of user-friendly snippets with pictograms
     "rafamadriz/friendly-snippets",
-		"onsails/lspkind.nvim",
+    "onsails/lspkind.nvim",
   },
-	config = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		local lspkind = require("lspkind")
+  config = function()
+  local cmp = require("cmp")
+  local luasnip = require("luasnip")
+  local lspkind = require("lspkind")
 
-		require("luasnip.loaders.from_vscode").lazy_load()
+  require("luasnip.loaders.from_vscode").lazy_load()
 
-		luasnip.config.setup {}
+  luasnip.config.setup {}
 
-		cmp.setup {
-		  snippet = {
-		    expand = function(args)
-		      luasnip.lsp_expand(args.body)
-		    end,
-		  },
-		  completion = {
-		    completeopt = "menu,menuone,noinsert",
-		  },
-		  mapping = cmp.mapping.preset.insert {
-		    ["<C-j>"] = cmp.mapping.select_next_item(),
-		    ["<C-k>"] = cmp.mapping.select_prev_item(),
-		    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-		    ["<C-u>"] = cmp.mapping.scroll_docs(4),
-		    ["<C-Space>"] = cmp.mapping.complete {},
-		    ["<CR>"] = cmp.mapping.confirm {
-		      behavior = cmp.ConfirmBehavior.Replace,
-		      select = true,
-		    },
-		    ["<Tab>"] = cmp.mapping(function(fallback)
-		      if cmp.visible() then
-		        cmp.select_next_item()
-		      elseif luasnip.expand_or_locally_jumpable() then
-		        luasnip.expand_or_jump()
-		      else
-		        fallback()
-		      end
-		    end, { "i", "s" }),
-		    ["<S-Tab>"] = cmp.mapping(function(fallback)
-		      if cmp.visible() then
-		        cmp.select_prev_item()
-		      elseif luasnip.locally_jumpable(-1) then
-		        luasnip.jump(-1)
-		      else
-		        fallback()
-		      end
-		    end, { "i", "s" }),
-		  },
-		  sources = {
-		    { name = "nvim_lsp" },
-		    { name = "luasnip" },
-				{ name = "buffer" },
-		    { name = "path" },
-		  },
+  cmp.setup {
+    snippet = {
+      expand = function(args)
+        luasnip.lsp_expand(args.body)
+      end,
+    },
+    completion = {
+      completeopt = "menu,menuone,noinsert",
+    },
+    mapping = cmp.mapping.preset.insert {
+      ["<C-j>"] = cmp.mapping.select_next_item(),
+      ["<C-k>"] = cmp.mapping.select_prev_item(),
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-u>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete {},
+      ["<CR>"] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = true,
+      },
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expand_or_locally_jumpable() then
+          luasnip.expand_or_jump()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.locally_jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+    },
+    sources = {
+      { name = "nvim_lsp" },
+      { name = "luasnip" },
+      { name = "buffer" },
+      { name = "path" },
+    },
       formatting = {
         format = lspkind.cmp_format({
           maxwidth = 50,
           ellipsis_char = "..",
         }),
       },
-		}
-	end,
+  }
+  end,
 }
 
