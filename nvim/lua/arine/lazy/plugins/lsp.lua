0ea1e5e3b8002bdc6e18448d46016401c1172c42
Arine <arine@home.local>
2023-12-18 02:12:55 +0900

    Fix typos

--- nvim/lua/arine/lazy/plugins/lsp.lua
+++ nvim/lua/arine/lazy/plugins/lsp.lua
@@ -113,19 +113,19 @@ return {
     -- Ensure the servers above are installed
     local mason_lspconfig = require("mason-lspconfig")
 
-    mason_lspconfig.setup {
+    mason_lspconfig.setup({
       ensure_installed = vim.tbl_keys(servers),
       automatic_installation = true,
-    }
-    mason_lspconfig.setup_handlers {
+    })
+    mason_lspconfig.setup_handlers({
       function(server_name)
-        require("lspconfig")[server_name].setup {
+        require("lspconfig")[server_name].setup({
           capabilities = capabilities,
           on_attach = on_attach,
           settings = servers[server_name],
           filetypes = (servers[server_name] or {}).filetypes,
-        }
+        })
       end,
-    }
+    })
   end,
 }
