abb78ffbfdb987d72a2ed38bfb98cb815000db90
Arine <arine@home.local>
2023-12-19 03:33:47 +0900

    Configured from LazyVim

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,13 @@
+return {
+  -- change some telescope options and a keymap to browse plugin files
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+}
