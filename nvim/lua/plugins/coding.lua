7c07c8cbfdf9a155afa024194904f790e76fd637
Arine <peppydays@gmail.com>
2023-12-21 23:51:37 +0900

    Still configuring ..

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -5,4 +5,73 @@ return {
     cmd = "IncRename",
     config = true,
   },
+  -- Ensure LSP installed
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "rust-analyzer",
+      })
+    end,
+  },
+  -- LSP config
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = true },
+      yamlls = {
+        settings = {
+          yaml = {
+            keyOrdering = false,
+          },
+        },
+      },
+    },
+  },
+  {
+    "nvim-treesitter/playground",
+    cmd = "TSPlaygroundToggle",
+  },
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "fish",
+        "gitignore",
+        "http",
+        "java",
+        "rust",
+        "sql",
+        "python",
+      },
+      -- matchup = {
+      -- 	enable = true,
+      -- },
+      -- https://github.com/nvim-treesitter/playground#query-linter
+      query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = { "BufWrite", "CursorHold" },
+      },
+      playground = {
+        enable = true,
+        disable = {},
+        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+        persist_queries = true, -- Whether the query persists across vim sessions
+        keybindings = {
+          toggle_query_editor = "o",
+          toggle_hl_groups = "i",
+          toggle_injected_languages = "t",
+          toggle_anonymous_nodes = "a",
+          toggle_language_display = "I",
+          focus_language = "f",
+          unfocus_language = "F",
+          update = "R",
+          goto_node = "<cr>",
+          show_help = "?",
+        },
+      },
+    },
+  },
 }
