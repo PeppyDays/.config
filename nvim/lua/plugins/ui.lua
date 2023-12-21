64d4d626518629955a3ce8cefa7a160efc61f8d6
Arine <arine@home.local>
2023-12-22 01:48:47 +0900

    Removed tabs for buffer

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -65,10 +65,10 @@ return {
   {
     "rcarriga/nvim-notify",
     opts = {
-      timeout = 5000,
+      timeout = 3000,
     },
   },
-  -- animations
+  -- animations when jumping a cursor
   {
     "echasnovski/mini.animate",
     event = "VeryLazy",
@@ -79,20 +79,20 @@ return {
     end,
   },
   -- buffer line to show only tabs, not buffers
-  -- {
-  --   "akinsho/bufferline.nvim",
-  --   event = "VeryLazy",
-  --   keys = {
-  --     { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-  --     { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-  --   },
-  --   opts = {
-  --     options = {
-  --       mode = "tabs",
-  --       -- separator_style = "slant",
-  --       show_buffer_close_icons = false,
-  --       show_close_icon = false,
-  --     },
-  --   },
-  -- },
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
+    opts = {
+      options = {
+        mode = "tabs",
+        separator_style = "slant",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
 }
