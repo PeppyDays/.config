48bd83286a184180abbe2fe12451cf551aaab3f3
Arine <peppydays@gmail.com>
2024-01-23 04:58:58 +0900

    Added go language setup in NeoVim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     -- { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.editor.symbols-outline" },
