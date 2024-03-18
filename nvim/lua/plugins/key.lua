755377378bf9f12918621a10175343578963d0f4
Arine <peppydays@gmail.com>
2024-03-18 22:28:51 +0900

    Allow arrow keys in vim

--- nvim/lua/plugins/key.lua
+++ nvim/lua/plugins/key.lua
@@ -4,7 +4,12 @@ return {
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
     opts = {
       max_count = 5,
-      disabled_keys = {},
+      disabled_keys = {
+        ["<Up>"] = {},
+        ["<Down>"] = {},
+        ["<Left>"] = {},
+        ["<Right>"] = {},
+      },
     },
   },
 }
