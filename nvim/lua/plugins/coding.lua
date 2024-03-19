2c1ff8daa2269a4fa4076ee486b2ea97e1678291
Arine <peppydays@gmail.com>
2024-03-20 02:22:22 +0900

    Added pkl treesitter

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -13,4 +13,21 @@ return {
       },
     },
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = function(_)
+      vim.cmd("TSUpdate")
+    end,
+  },
+  {
+    "https://github.com/apple/pkl-neovim",
+    lazy = true,
+    event = "BufReadPre *.pkl",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    build = function()
+      vim.cmd("TSInstall! pkl")
+    end,
+  },
 }
