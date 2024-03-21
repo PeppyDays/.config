5623d52ed1f1cf96c452cb90f1122ab8eb5fd341
Arine <peppydays@gmail.com>
2024-03-21 23:50:44 +0900

    Added inlay enabled

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -30,4 +30,37 @@ return {
       vim.cmd("TSInstall! pkl")
     end,
   },
+  -- inlay hint is enabled from neovim >= 0.10
+  -- enable it after upgrade
+  -- reference https://vinnymeller.com/posts/neovim_nightly_inlay_hints/
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^3",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "mfussenegger/nvim-dap",
+      {
+        "lvimuser/lsp-inlayhints.nvim",
+        opts = {},
+      },
+    },
+    ft = { "rust" },
+    config = function()
+      vim.g.rustaceanvim = {
+        inlay_hints = {
+          highlight = "NonText",
+        },
+        tools = {
+          hover_actions = {
+            auto_focus = true,
+          },
+        },
+        server = {
+          on_attach = function(client, bufnr)
+            require("lsp-inlayhints").on_attach(client, bufnr)
+          end,
+        },
+      }
+    end,
+  },
 }
