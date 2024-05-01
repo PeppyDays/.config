3d58b0c20198c7595b849768546f9d72d98bdf49
Arine <peppydays@gmail.com>
2024-05-02 02:58:33 +0900

    Removed pfetch config

--- fish/config.fish
+++ fish/config.fish
@@ -13,15 +13,6 @@ starship init fish | source
 # rust
 set -gx fish_user_paths $HOME/.cargo/bin $PATH
 
-# pfetch configuration
-set -gx PF_INFO ascii title os shell editor memory uptime
-set -gx PF_ASCII Catppuccin
-set -gx PF_COL1 9
-set -gx PF_COL2 9
-set -gx PF_COL3 3
-set -gx PF_ALIGN 10
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    pfetch
 end
