5756af45932c75df9b02f83d5b8101ae3b1ee43c
Arine <peppydays@gmail.com>
2024-05-14 01:38:00 +0900

    Enabled noice

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,11 +1,12 @@
 return {
   -- consider these later
   { "folke/flash.nvim", enabled = false },
+  -- { "folke/noice.nvim", enabled = false },
+  -- { "MunifTanjim/nui.nvim", enabled = false },
   -- maybe not used
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
-  { "folke/noice.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
   { "L3MON4D3/LuaSnip", enabled = false },
   { "dstein64/vim-startuptime", enabled = false },
@@ -13,6 +14,5 @@ return {
   { "saadparwaiz1/cmp_luasnip", enabled = false },
   { "folke/neodev.nvim", enabled = false },
   { "folke/neoconf.nvim", enabled = false },
-  { "MunifTanjim/nui.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
 }
