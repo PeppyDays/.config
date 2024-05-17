01cea6bfddbbded6cfeeaa7dbcae2c9f031c15c5
Arine <peppydays@gmail.com>
2024-05-17 13:29:39 +0900

    Added glance

---
+++ nvim/lua/plugins/glance.lua
@@ -0,0 +1,52 @@
+return {
+  "dnlhc/glance.nvim",
+  enabled = true,
+  event = "LspAttach",
+  config = function()
+    local actions = require("glance").actions
+
+    require("glance").setup({
+      border = {
+        enable = true, -- Show window borders. Only horizontal borders allowed
+      },
+      winbar = {
+        enable = false, -- Available strating from nvim-0.8+
+      },
+      mappings = {
+        list = {
+          ["<C-l>"] = actions.enter_win("preview"), -- Focus preview window
+        },
+        preview = {
+          ["<C-l>"] = actions.enter_win("list"), -- Focus list window
+        },
+      },
+    })
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      desc = "Set Glance.nvim mappings",
+      callback = function(event)
+        -- `defer_fn` here because must be set after `on_attach` lsp-config method
+        vim.defer_fn(function()
+          if vim.api.nvim_buf_is_valid(event.buf) then
+            vim.keymap.set("n", "gd", "<Cmd>Glance definitions<Cr>", {
+              buffer = event.buf,
+              desc = "Glance definitions",
+            })
+            vim.keymap.set("n", "gi", "<Cmd>Glance implementations<Cr>", {
+              buffer = event.buf,
+              desc = "Glance implementations",
+            })
+            vim.keymap.set("n", "gr", "<Cmd>Glance references<Cr>", {
+              buffer = event.buf,
+              desc = "Glance references",
+            })
+            vim.keymap.set("n", "go", "<Cmd>Glance type_definitions<Cr>", {
+              buffer = event.buf,
+              desc = "Glance type definitions",
+            })
+          end
+        end, 100)
+      end,
+    })
+  end,
+}
