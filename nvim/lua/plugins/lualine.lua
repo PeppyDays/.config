734949e9d2edef4bf3767a2526fa96b97d59675e
Arine <peppydays@gmail.com>
2024-06-18 00:59:59 +0900

    Removed lualine z for datetime

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -9,18 +9,10 @@ return {
     },
     sections = {
       lualine_z = {
-        function()
-          return os.date("%R")
-        end,
+        -- function()
+        --   return "Arine"
+        -- end,
       },
     },
   },
-  -- opts = function(_, opts)
-  --   table.insert(opts.sections.lualine_z, "😄")
-  --   table.insert(opts.options, {
-  --     icons_enabled = false,
-  --     component_separators = "|",
-  --     section_separators = "",
-  --   })
-  -- end,
 }
