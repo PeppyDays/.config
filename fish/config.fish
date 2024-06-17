8e9c91b15d8ad24e094123e1986e9f2f67f1f7eb
Arine <peppydays@gmail.com>
2024-06-18 00:59:40 +0900

    Added yazi command

--- fish/config.fish
+++ fish/config.fish
@@ -16,6 +16,16 @@ starship init fish | source
 # rust
 set -gx fish_user_paths $HOME/.cargo/bin $PATH
 
+# yazi
+function yy
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
