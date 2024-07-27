5086ee449b51864bcc25d03a0e5b2502c8aa6721
Arine <peppydays@gmail.com>
2024-07-28 02:28:32 +0900

    Cleanup fish config

--- fish/config.fish
+++ fish/config.fish
@@ -5,30 +5,20 @@ fish_add_path /opt/homebrew/sbin
 # go bin directory
 fish_add_path ~/go/bin
 
+# rust bin directory
+set -gx fish_user_paths $HOME/.cargo/bin $PATH
+
 # greeting message
 set fish_greeting
 
-# default editor to wrapper of nvim
-set -gx EDITOR nvim
+# default editor to wrapper of helix
+set -gx EDITOR hx
 
 # starship prompt configuration
 starship init fish | source
 
 # zoxide prompt configuration
 zoxide init fish | source
-
-# rust
-set -gx fish_user_paths $HOME/.cargo/bin $PATH
-
-# yazi
-function yy
-    set tmp (mktemp -t "yazi-cwd.XXXXXX")
-    yazi $argv --cwd-file="$tmp"
-    if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-        cd -- "$cwd"
-    end
-    rm -f -- "$tmp"
-end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
