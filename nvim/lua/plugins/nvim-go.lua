93a9b11520ab682076af7c148c5a492e443c163c
Arine <peppydays@gmail.com>
2024-07-30 02:07:59 +0900

    Disabled nvim-go

--- nvim/lua/plugins/nvim-go.lua
+++ nvim/lua/plugins/nvim-go.lua
@@ -1,23 +1,24 @@
-return {
-  "crispgm/nvim-go",
-  ft = "go",
-  config = function()
-    require("go").setup({
-      auto_lint = true,
-      linter = "golangci-lint",
-      auto_format = true,
-      formatter = "gofumpt",
-    })
-
-    vim.keymap.set("n", "<Leader>cge", "<cmd>GoIfErr<cr>", { desc = "Put if clause for error handling" })
-
-    local NvimGo = vim.api.nvim_create_augroup("NvimGo", {
-      clear = true,
-    })
-    vim.api.nvim_create_autocmd({ "User" }, {
-      pattern = "NvimGoLintPopupPost",
-      group = NvimGo,
-      command = "wincmd p",
-    })
-  end,
-}
+return {}
+-- return {
+--   "crispgm/nvim-go",
+--   ft = "go",
+--   config = function()
+--     require("go").setup({
+--       auto_lint = true,
+--       linter = "golangci-lint",
+--       auto_format = true,
+--       formatter = "gofumpt",
+--     })
+--
+--     vim.keymap.set("n", "<Leader>cge", "<cmd>GoIfErr<cr>", { desc = "Put if clause for error handling" })
+--
+--     local NvimGo = vim.api.nvim_create_augroup("NvimGo", {
+--       clear = true,
+--     })
+--     vim.api.nvim_create_autocmd({ "User" }, {
+--       pattern = "NvimGoLintPopupPost",
+--       group = NvimGo,
+--       command = "wincmd p",
+--     })
+--   end,
+-- }
