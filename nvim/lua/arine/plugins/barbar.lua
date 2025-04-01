eb625b6001a7b5b7d1b5400dd20de264384f6d13
Arine <peppydays@gmail.com>
2025-04-02 00:04:45 +0900

    Disabled barbar

--- nvim/lua/arine/plugins/barbar.lua
+++ nvim/lua/arine/plugins/barbar.lua
@@ -1,5 +1,6 @@
 return {
   'romgrk/barbar.nvim',
+  enabled = false,
   dependencies = {
     'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
     'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
@@ -12,8 +13,8 @@ return {
     map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
     map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
     -- Re-order to previous/next
-    map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
-    map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+    -- map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+    -- map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
     -- Goto buffer in position...
     map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
     map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
@@ -28,8 +29,8 @@ return {
     -- Pin/unpin buffer
     map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
     -- Close buffer
-    -- map('n', '<A-x>', '<Cmd>BufferClose<CR>', opts)
-    map('n', '<A-t>', '<Cmd>BufferRestore<CR>', opts)
+    map('n', '<Space>bd', '<Cmd>BufferClose<CR>', opts)
+    -- map('n', '<A-t>', '<Cmd>BufferRestore<CR>', opts)
     -- map('n', '<leader>bx', '<Cmd>BufferCloseAllButCurrentOrPinned<CR>', opts)
 
     map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
