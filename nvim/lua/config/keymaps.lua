7175e2d53f3f7965c3b10175fcefbf32cd54df31
Arine <peppydays@gmail.com>
2025-04-06 03:15:19 +0900

    Updated nvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,6 +10,11 @@ vim.keymap.del("n", "<leader>`")
 vim.keymap.del("n", "<leader>L")
 vim.keymap.del("n", "<leader>S")
 
+-- move like helix
+vim.keymap.set("n", "gh", "0", { desc = "Go to line start" })
+vim.keymap.set("n", "gl", "$", { desc = "Go to line end " })
+vim.keymap.set("n", "gs", "^", { desc = "Go to first non-blank in line" })
+
 -- exit with jk
 vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,9 +1,3 @@
--- local signs = { Error = "e", Warn = "w", Hint = "h", Info = "i" }
--- for type, icon in pairs(signs) do
---   local hl = "DiagnosticSign" .. type
---   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
--- end
-
 return {
   {
     "neovim/nvim-lspconfig",
