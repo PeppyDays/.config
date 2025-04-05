e690f0e2e84e6b710909ffaff6982feed42371bd
Arine <peppydays@gmail.com>
2025-04-05 23:52:47 +0900

    Updated nvim

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -20,4 +20,31 @@ return {
       },
     },
   },
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      -- mappings = {
+      --   add = "ms",
+      --   delete = "md",
+      --   find = "mf",
+      --   find_left = "mF",
+      --   highlight = "mh",
+      --   replace = "mr",
+      --   update_n_lines = "mn",
+      -- },
+    },
+  },
+  {
+    "echasnovski/mini.pairs",
+    -- Disabled because of difficulties for disabling single quote pairing onl in Rust
+    -- Switched to nvim-autopairs
+    enabled = false,
+    opts = {
+      mappings = {
+        ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\`].", register = { cr = false } },
+        ["<"] = { action = "open", pair = "<>", neigh_pattern = "[^\\]." },
+        [">"] = { action = "close", pair = "<>", neigh_pattern = "[^\\]." },
+      },
+    },
+  },
 }
