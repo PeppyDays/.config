4cd3f9e6e552e267b7a6e2cac75749adb951a583
Arine <peppydays@gmail.com>
2025-04-05 18:51:12 +0900

    Updated nvim

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,6 +1,23 @@
 return {
-  "echasnovski/mini.icons",
-  opts = {
-    style = "ascii",
+  {
+    "echasnovski/mini.icons",
+    opts = {
+      style = "ascii",
+    },
+  },
+  {
+    "echasnovski/mini.move",
+    opts = {
+      mappings = {
+        left = "<A-h>",
+        right = "<A-l>",
+        down = "<A-j>",
+        up = "<A-k>",
+        line_left = "<A-h>",
+        line_right = "<A-l>",
+        line_down = "<A-j>",
+        line_up = "<A-k>",
+      },
+    },
   },
 }
