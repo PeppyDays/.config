659ce943909336c71add2f4cdfaacf4ee88c77d3
Arine <peppydays@gmail.com>
2025-04-07 02:08:07 +0900

    Updated nvim

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,20 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  event = "BufReadPost",
+  opts = {
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      keymap = {
+        accept = "<Tab>",
+      },
+    },
+    panel = { enabled = false },
+    filetypes = {
+      markdown = true,
+      help = true,
+    },
+  },
+}
