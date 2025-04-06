1e90c260436221ae45dbcf02fb7178aa07b1fca2
Arine <peppydays@gmail.com>
2025-04-07 00:41:58 +0900

    Updated nvim

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,6 +2,106 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      codelens = {
+        enabled = true,
+      },
+      showMessage = {
+        messageActionItem = {
+          additionalPropertiesSupport = true,
+        },
+      },
+      servers = {
+        -- rust-analyzer's configuration is set in rustaceanvim.lua
+        -- If not using rustaceanvim anymore, set it here again
+        -- rust_analyzer = {
+        --   settings = {
+        --     ["rust-analyzer"] = {
+        --       cargo = {
+        --         allFeatures = true,
+        --         loadOutDirsFromCheck = true,
+        --         buildScripts = {
+        --           enable = true,
+        --         },
+        --       },
+        --       check = {
+        --         command = "clippy",
+        --         extraArgs = { "--no-deps" },
+        --       },
+        --       checkOnSave = true,
+        --       diagnostics = {
+        --         enable = true,
+        --       },
+        --       procMacro = {
+        --         enable = true,
+        --         ignored = {
+        --           ["async-trait"] = { "async_trait" },
+        --           ["napi-derive"] = { "napi" },
+        --           ["async-recursion"] = { "async_recursion" },
+        --         },
+        --       },
+        --       imports = {
+        --         preferPrelude = true,
+        --         granularity = {
+        --           group = "item",
+        --           enforce = true,
+        --         },
+        --         prefix = "crate",
+        --       },
+        --       files = {
+        --         excludeDirs = {
+        --           ".direnv",
+        --           ".git",
+        --           ".github",
+        --           ".gitlab",
+        --           "bin",
+        --           "node_modules",
+        --           "target",
+        --           "venv",
+        --           ".venv",
+        --         },
+        --       },
+        --     },
+        --   },
+        -- },
+        gopls = {
+          settings = {
+            gopls = {
+              gofumpt = true,
+              codelenses = {
+                gc_details = false,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              analyses = {
+                fieldalignment = true,
+                nilness = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+              },
+              usePlaceholders = true,
+              completeUnimported = true,
+              staticcheck = true,
+              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules", "-.nvim" },
+              semanticTokens = true,
+            },
+          },
+        },
+      },
       diagnostics = {
         signs = {
           text = {
---
+++ nvim/lua/plugins/rustaceanvim.lua
@@ -0,0 +1,55 @@
+return {
+  "mrcjkb/rustaceanvim",
+  opts = {
+    server = {
+      default_settings = {
+        ["rust-analyzer"] = {
+          cargo = {
+            allFeatures = true,
+            loadOutDirsFromCheck = true,
+            buildScripts = {
+              enable = true,
+            },
+          },
+          check = {
+            command = "clippy",
+            extraArgs = { "--no-deps" },
+          },
+          checkOnSave = true,
+          diagnostics = {
+            enable = true,
+          },
+          procMacro = {
+            enable = true,
+            ignored = {
+              ["async-trait"] = { "async_trait" },
+              ["napi-derive"] = { "napi" },
+              ["async-recursion"] = { "async_recursion" },
+            },
+          },
+          imports = {
+            preferPrelude = true,
+            granularity = {
+              group = "item",
+              enforce = true,
+            },
+            prefix = "crate",
+          },
+          files = {
+            excludeDirs = {
+              ".direnv",
+              ".git",
+              ".github",
+              ".gitlab",
+              "bin",
+              "node_modules",
+              "target",
+              "venv",
+              ".venv",
+            },
+          },
+        },
+      },
+    },
+  },
+}
