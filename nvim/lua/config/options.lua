21b7e3b67592790d56ed5b38b537dcc671b1b5d6
Arine <peppydays@gmail.com>
2025-04-08 00:11:43 +0900

    Updated nvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,10 +2,22 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- line numbers
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+-- tabs and indentation
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
 -- Disable snacks animation
 vim.g.snacks_animate = false
 
--- Modify folding characters
+-- Modify folding characters to ascii-compatible
 vim.opt.fillchars:append({
   foldclose = ">",
 })
