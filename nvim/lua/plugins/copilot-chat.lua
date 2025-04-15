6d461625e6d9621b45a914e17fbbecb3805b0b96
Arine <peppydays@gmail.com>
2025-04-16 02:52:35 +0900

    Fixed github copilot chat config

--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -1,6 +1,11 @@
 return {
   "CopilotC-Nvim/CopilotChat.nvim",
   opts = {
-    model = "claude-3.7-sonnet-thought",
+    model = "gpt-4o",
+    auto_insert_mode = false,
+    insert_at_end = true,
+    question_header = "Q: Arine ",
+    answer_header = "A: Copilot ",
+    error_header = "E: Error ",
   },
 }
