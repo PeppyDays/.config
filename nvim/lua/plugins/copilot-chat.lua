b6009b6e895d036e41e54fd3a65d3c8cd7ad059a
Arine <peppydays@gmail.com>
2025-04-16 23:01:52 +0900

    Updated gpt model

--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -1,9 +1,8 @@
 return {
   "CopilotC-Nvim/CopilotChat.nvim",
   opts = {
-    model = "gpt-4o",
+    model = "gpt-4.1",
     auto_insert_mode = false,
-    insert_at_end = true,
     question_header = "Q: Arine ",
     answer_header = "A: Copilot ",
     error_header = "E: Error ",
