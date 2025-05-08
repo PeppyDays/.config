07ede0319011c01268f018dcad7a954f5728b79c
Arine <peppydays@gmail.com>
2025-05-08 23:03:26 +0900

    Updated nvim

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,5 +1,16 @@
 -- TODO: Remove this after LazyVim supports Mason v2, check https://github.com/LazyVim/LazyVim/issues/6039
 return {
-  { "mason-org/mason.nvim", version = "^1.0.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+  {
+    "mason-org/mason.nvim",
+    version = "^1.0.0",
+    opts = {
+      ensure_installed = {
+        "prettier",
+      },
+    },
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    version = "^1.0.0",
+  },
 }
