bb29328687cb53ee6a1e9e7f5202873a36d2b305
Arine <peppydays@gmail.com>
2025-05-09 15:04:52 +0900

    Updated nvim

---
+++ nvim/lua/plugins/neotest.lua
@@ -0,0 +1,42 @@
+return {
+  "nvim-neotest/neotest",
+  config = function(_, opts)
+    opts.icons = {
+      running_animated = {
+        "⠋",
+        "⠙",
+        "⠚",
+        "⠒",
+        "⠂",
+        "⠂",
+        "⠒",
+        "⠲",
+        "⠴",
+        "⠦",
+        "⠖",
+        "⠒",
+        "⠐",
+        "⠐",
+        "⠒",
+        "⠓",
+        "⠋",
+      },
+      passed = "P",
+      running = "R",
+      failed = "F",
+      skipped = "S",
+      unknown = "U",
+      non_collapsible = "─",
+      collapsed = "─",
+      expanded = "╮",
+      child_prefix = "├",
+      final_child_prefix = "╰",
+      child_indent = "│",
+      final_child_indent = " ",
+      watching = "W",
+      test = "T",
+      notify = "N",
+    }
+    require("neotest").setup(opts)
+  end,
+}
