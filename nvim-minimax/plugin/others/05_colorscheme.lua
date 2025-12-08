5b5a7dd836a54f79f424089d1b20344c7c4ba75d
Arine <peppydays@gmail.com>
2025-12-08 21:12:07 +0900

    Changed the theme to catppuccin frappe

---
+++ nvim-minimax/plugin/others/05_colorscheme.lua
@@ -0,0 +1,18 @@
+-- Enable 'catppuccin' color scheme with 'frappe' flavor.
+--
+-- See also:
+-- - https://github.com/catppuccin/nvim - catppuccin documentation
+-- - `:h catppuccin` - catppuccin help page
+-- - `:h mini.nvim-color-schemes` - list of mini.nvim color schemes
+
+local now, add = MiniDeps.now, MiniDeps.add
+
+now(function()
+	add("catppuccin/nvim")
+	vim.cmd("colorscheme catppuccin-frappe")
+end)
+
+-- You can try these other catppuccin flavors (uncomment with `gcc`):
+-- now(function() vim.cmd('colorscheme catppuccin-latte') end)  -- light
+-- now(function() vim.cmd('colorscheme catppuccin-macchiato') end)
+-- now(function() vim.cmd('colorscheme catppuccin-mocha') end)
