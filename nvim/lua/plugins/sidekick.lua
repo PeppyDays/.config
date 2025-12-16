d663e2c8d91c78f90f1dfe49c3b3a9208ef02b23
Arine <peppydays@gmail.com>
2025-12-17 01:14:56 +0900

    Rollback to zellij

--- nvim/lua/plugins/sidekick.lua
+++ nvim/lua/plugins/sidekick.lua
@@ -2,10 +2,10 @@ return {
   "folke/sidekick.nvim",
   opts = {
     cli = {
-      -- mux = {
-      --   backend = "zellij",
-      --   enabled = true,
-      -- },
+      mux = {
+        backend = "zellij",
+        enabled = true,
+      },
       tools = {
         claude = {
           cmd = { "claude", "--dangerously-skip-permissions" },
