6bcc8bd61dc542e6c61dc5d629bd78cd6c19faa2
Arine <peppydays@gmail.com>
2025-12-19 00:59:32 +0900

    Changed the icon of sidekick AI picker

--- nvim/lua/plugins/sidekick.lua
+++ nvim/lua/plugins/sidekick.lua
@@ -1,6 +1,18 @@
 return {
   "folke/sidekick.nvim",
   opts = {
+    ui = {
+      icons = {
+        attached = "o ",
+        started = "o ",
+        installed = "o ",
+        missing = "x ",
+        external_attached = "o ",
+        external_started = "o ",
+        terminal_attached = "o ",
+        terminal_started = "o ",
+      },
+    },
     cli = {
       mux = {
         backend = "zellij",
